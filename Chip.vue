6d01ab2cf931c11b32bdbcf2f893ae151fa1f0b9
Aiken Drum <eikendram@gmail.com>
2023-09-15 08:52:37 +0300

    initial

---
+++ Chip.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-chip :color="getColor()" :text-color="getTextColor()">
+    <slot></slot>
+  </v-chip>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  type: "error" | "info" | "success" | "warning" | undefined;
+}>();
+
+function getColor() {
+  if (props.type) {
+    switch (props.type) {
+      case "error":
+        return "red";
+      case "info":
+        return "primary";
+      case "success":
+        return "green";
+      case "warning":
+        return "orange";
+    }
+  }
+}
+
+function getTextColor() {
+  return props.type ? "white" : null;
+}
+</script>
